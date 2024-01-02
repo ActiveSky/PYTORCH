e8128dd7e4151bb8fbeb66f35c24fd907edf39b1
sky <94363281+ActiveSky@users.noreply.github.com>
2024-01-02 07:48:26 +0000

    Add tensorboardX support for visualizing image grid

--- prepare_data.py
+++ prepare_data.py
@@ -2,9 +2,13 @@
 
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
+
+# # set the writer for tensorboardX object
+# writer = SummaryWriter('./pytorch_tb')
 
 # set the super parameters
-
 batch_size_train = 64
 batch_size_test = 1000
 
@@ -34,3 +38,13 @@ def prepare_data():
     test_loader = DataLoader(testset, batch_size=batch_size_test, shuffle=False)
 
     return train_loader, test_loader
+
+if __name__ == "__main__":
+    train_loader, test_loader = prepare_data()
+    images, labels = next(iter(train_loader))
+    # 将多张图片拼接成一张图片，中间用黑色网格分割
+    # create grid of images
+    writer = SummaryWriter('./pytorch_tb')
+    img_grid = torchvision.utils.make_grid(images)
+    writer.add_image('image_grid', img_grid)
+    writer.close()
